05e8ffffb55b2495157dbaff7eb1d98cb7e39a20
John Doe <john.doe@gmail.com>
2025-06-08 11:20:14 +0200

    fix unbound

--- install.sh
+++ install.sh
@@ -29,6 +29,9 @@ readonly RED='\033[0;31m'
 readonly GREEN='\033[0;32m'
 readonly YELLOW='\033[1;33m'
 readonly BLUE='\033[0;34m'
+readonly CYAN='\033[0;36m'
+readonly BOLD='\033[1m'
+readonly DIM='\033[2m'
 readonly NC='\033[0m' # No Color
 
 # Symbols
